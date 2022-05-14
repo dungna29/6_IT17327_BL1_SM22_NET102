594c7778c643072662b969f8a7694c421238a23b
Dungna89 <dungna29@fpt.edu.vn>
2022-05-14 13:07:37 +0700

    1

--- 6_IT17327_BL1_SM22_NET102/BAI_1_0_ONTAP_NET101_CRUD/ChoService.cs
+++ 6_IT17327_BL1_SM22_NET102/BAI_1_0_ONTAP_NET101_CRUD/ChoService.cs
@@ -29,13 +29,12 @@ namespace BAI_1_0_ONTAP_NET101_CRUD
         //Code 10 phút code chức năng thêm
         public void ThemCho()
         {
-           
+
             _input = GetInputValue("số lượng");
             for (int i = 0; i < Convert.ToInt32(_input); i++)
             {
                 _cho = new Cho();
-                Console.WriteLine("Mời bạn nhập id: ");
-                _cho.Id = Convert.ToInt32(Console.ReadLine());
+                _cho.Id = GetIDCho();
                 Console.WriteLine("Mời bạn nhập tên chó: ");
                 _cho.Ten = Console.ReadLine();
                 Console.WriteLine("Mời bạn nhập giới tính: (1 - Đức | 0 - Cái)");
@@ -46,6 +45,110 @@ namespace BAI_1_0_ONTAP_NET101_CRUD
                 _cho.Mau = Convert.ToInt32(Console.ReadLine());
                 _lstChos.Add(_cho);
             }
+        }
+        //Tìm kiếm, Sửa , Xóa =
+        public void TimKiem()
+        {
+            // Console.WriteLine("Nhập Id chó: ");
+            // _input = Console.ReadLine();
+            // for (int i = 0; i < _lstChos.Count; i++)
+            // {
+            //     if (_lstChos[i].Id == Convert.ToInt32(_input))
+            //     {
+            //         _lstChos[i].InRaManHinh();
+            //         return;
+            //     }
+            // }
+            //
+            // Console.WriteLine("Không tìm thấy");
+            var temp = GetIndexById();
+            if (temp == -1)
+            {
+                Console.WriteLine("Không tìm thấy");
+                return;
+            } 
+            _lstChos[temp].InRaManHinh();
+        }
+        public void Xoa()
+        {
+            // Console.WriteLine("Nhập Id chó: ");
+            // _input = Console.ReadLine();
+            // for (int i = 0; i < _lstChos.Count; i++)
+            // {
+            //     if (_lstChos[i].Id == Convert.ToInt32(_input))
+            //     {
+            //         _lstChos.RemoveAt(i);
+            //         Console.WriteLine("Xóa thành công");
+            //         return;
+            //     }
+            // }
+            //
+            // Console.WriteLine("Không tìm thấy");
+            var temp = GetIndexById();
+            if (temp == -1)
+            {
+                Console.WriteLine("Không tìm thấy");
+                return;
+            }
+            _lstChos.RemoveAt(temp);
+        }
+        public void Sua()
+        {
+            // Console.WriteLine("Nhập Id chó: ");
+            // _input = Console.ReadLine();
+            // for (int i = 0; i < _lstChos.Count; i++)
+            // {
+            //     if (_lstChos[i].Id == Convert.ToInt32(_input))
+            //     {
+            //         Console.WriteLine("Mời bạn nhập tên: ");
+            //         _lstChos[i].Ten = Console.ReadLine();
+            //         Console.WriteLine("Sửa thành công");
+            //         return;
+            //     }
+            // }
+            //
+            // Console.WriteLine("Không tìm thấy");
+            var temp = GetIndexById();
+            if (temp == -1)
+            {
+                Console.WriteLine("Không tìm thấy");
+                return;
+            }
+            Console.WriteLine("Mời bạn nhập tên: ");
+            _lstChos[temp].Ten = Console.ReadLine();
+            Console.WriteLine("Sửa thành công");
+        }
+
+        public void InDs(){
+            foreach (var x in _lstChos)
+            {
+                x.InRaManHinh();
+            }      
+        }
+
+        public int GetIDCho()
+        {
+            if (_lstChos.Count<0)
+            {
+                return 1;
+            }
+            return _lstChos.Max(c => c.Id) + 1;
+        }
+
+        public int GetIndexById()
+        {
+            Console.WriteLine("Nhập Id chó: ");
+            _input = Console.ReadLine();
+            // for (int i = 0; i < _lstChos.Count; i++)
+            // {
+            //     if (_lstChos[i].Id == Convert.ToInt32(_input))
+            //     {
+            //         return i;
+            //     }
+            // }
+            //
+            // return -1;
+            return _lstChos.FindIndex(c => c.Id == Convert.ToInt32(_input));
         }
 
         public String GetInputValue(string msg)
--- 6_IT17327_BL1_SM22_NET102/BAI_1_0_ONTAP_NET101_CRUD/Program.cs
+++ 6_IT17327_BL1_SM22_NET102/BAI_1_0_ONTAP_NET101_CRUD/Program.cs
@@ -13,6 +13,39 @@ namespace BAI_1_0_ONTAP_NET101_CRUD
          */
         static void Main(string[] args)
         {
+            ChoService cs = new ChoService();
+            string input;
+            do
+            {
+                Console.WriteLine("1. Thêm");
+                Console.WriteLine("2. Sửa");
+                Console.WriteLine("3. Xóa");
+                Console.WriteLine("4. Xuất ds");
+                Console.WriteLine("5. Tìm kiếm");
+                Console.WriteLine("6. Thoát");
+                Console.WriteLine("Mời bạn chọn chức năng: ");
+                input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        cs.ThemCho();
+                        break;
+                    case "2":
+                        cs.Sua();
+                        break;
+                    case "3":
+                        cs.Xoa();
+                        break;
+                    case "4":
+                        cs.InDs();
+                        break;
+                    case "5":
+                        cs.TimKiem();
+                        break;
+                    default:
+                        break;
+                }
+            } while (input != "6");
         }
     }
 }
