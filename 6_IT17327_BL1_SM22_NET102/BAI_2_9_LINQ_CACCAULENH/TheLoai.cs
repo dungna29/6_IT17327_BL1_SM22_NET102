706809b5a9ab3540b1c2bfb118e0435a3404fbe1
Dungna89 <dungna29@fpt.edu.vn>
2022-06-07 14:00:32 +0700

    1

---
+++ 6_IT17327_BL1_SM22_NET102/BAI_2_9_LINQ_CACCAULENH/TheLoai.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAI_2_9_LINQ_CACCAULENH
+{
+    class TheLoai
+    {
+        public int Id { get; set; }
+        public string MaTheLoai { get; set; }
+        public string TenTheLoai { get; set; }
+        public bool TrangThai { get; set; }
+
+     
+
+        public void InRaManHinh()
+        {
+            Console.WriteLine($"{Id} | {MaTheLoai} | {TenTheLoai} | {TrangThai}");
+        }
+    }
+}
